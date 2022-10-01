3b79946931430416e30b94ab5e12f675cf49a1d2
Seun-Ayela <seunayela@gmail.com>
2022-09-30 21:12:04 -0700

    added new changes

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * main - entry point
+ * @argc: function to count arguments
+ * @argv: array of points to strings
+ * Return: 0
+ */
+
+int main(int argc, char *argv[])
+{
+	(void) argc;
+	printf("%s\n", argv[0]);
+
+	return (0);
+}
